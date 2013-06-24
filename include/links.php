d79a71eb288b41985c8f3431cafc04f7c3595e70
randwsw <epecoom@gmail.com>
2013-06-24 15:34:53 +0200

    linki

--- include/links.php
+++ include/links.php
@@ -2,6 +2,8 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 	<script type="text/javascript" src="src/coin-slider.min.js"></script>
+	<script type="text/javascript" src="src/jquery.watermark.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="template/style.css">
 	<link rel="stylesheet" href="template/coin-slider-styles.css" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/plain; charset=utf-8" />
 <!-- ------------------------------------------------------------------------ -->
--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		
-		<!-- Links ------------------------------------------------------------------ -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-		<script type="text/javascript" src="src/coin-slider.min.js"></script>
-		<script type="text/javascript" src="src/jquery.watermark.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="template/style.css">
-		<link rel="stylesheet" href="template/coin-slider-styles.css" type="text/css" />
-		<meta http-equiv="Content-Type" content="text/plain; charset=utf-8" />
-		<!-- ------------------------------------------------------------------------ -->
-		
-		
+
 		<!-- Include links ---------------------------------------------------------- -->
 		<?php include 'include/links.php'; ?>
 		<!-- ------------------------------------------------------------------------ -->
-		
-		
-		
-		
+
 		<!-- Functions -------------------------------------------------------------- -->
 		<script type="text/javascript">
 		// Check hashtag -------------------------------------------- //
@@ -192,7 +178,10 @@
 			$('#grayout').css({"display":"none"});
 			$('#loginForm').css({"display":"none"});
 		});
-		$('#fname').watermark("Wpisz login");
+		
+		 $('#fname').watermark("Wpisz login");
+		
+		alert("asdasdasdasdasdasdasdas");
 		$('#tbPassword').watermark("Wpisz hasło");
 		$('#tbPassword2').watermark("Potwierdź hasło");
 	});
