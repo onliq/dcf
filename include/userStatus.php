f1c62f6f2146eba99bb2a2598dedbb337808d20a
randwsw <epecoom@gmail.com>
2013-06-24 15:47:03 +0200

    nowy stary index

--- include/userStatus.php
+++ include/userStatus.php
@@ -1,3 +1,28 @@
+<script type="text/javascript">
+// Login ---------------------------------------------------- //
+function grayout() {
+	
+		$('#grayout').css({"display":"none"});
+		$('#loginForm').css({"display":"none"});
+		
+		$("#loginButton").click(function(){
+			$('#grayout').css({"display":""});
+			$('#loginForm').css({"display":""});
+		});
+		
+		$("#grayout").click(function(){
+			$('#grayout').css({"display":"none"});
+			$('#loginForm').css({"display":"none"});
+		});
+		
+		$('#fname').watermark("Wpisz login");
+		$('#tbPassword').watermark("Wpisz hasło");
+		$('#tbPassword2').watermark("Potwierdź hasło");
+}
+                    // ---------------------------------------------------------- //
+</script>
+     
 <div id="userTopBar">
 	<a id="loginButton">Logowanie</a><a id="registerButton">Rejestracja</a>
 </div>
+
